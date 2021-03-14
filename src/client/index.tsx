97a8e140c3034ff73d8bb4081bf5dddb48c69801
Sukeroku Hanakawado <osushi.suke6@gmail.com>
2021-03-15 01:59:48 +0900

    Add Drag and drop the image

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,6 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-console.log('Hello react');
-
-ReactDOM.render(<App />, document.getElementById('helloReact'));
+ReactDOM.render(<App />, document.getElementById('dropzone'));

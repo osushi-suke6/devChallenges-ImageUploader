b3183dc08847a344a0eac071f7a530a6199057be
Sukeroku Hanakawado <osushi.suke6@gmail.com>
2021-03-14 22:12:36 +0900

    Disable /test

--- src/server/routes/indexRoutes.ts
+++ src/server/routes/indexRoutes.ts
@@ -4,10 +4,6 @@ const router: FastifyPluginAsync = async (app) => {
     app.get('/', async (_req, reply) => {
         reply.view('./views/index.pug');
     });
-
-    app.get('/test', async (_req, reply) => {
-        reply.view('./views/test.pug');
-    });
 }
 
 export default router;

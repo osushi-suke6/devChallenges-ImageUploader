1ab8a2fd7cc735b2d448b083a32f27acf3fb703b
Sukeroku Hanakawado <osushi.suke6@gmail.com>
2021-03-15 23:18:40 +0900

    Add ChooseAFileButton component

---
+++ src/client/components/ChooseAFileButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ChooseAFileButton = () => {
+
+    return (
+        <button className="chooseAFileButton">
+            <p>Choose a file</p>
+        </button>
+    );
+};
+
+export default ChooseAFileButton;
--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Dropzone from './components/Dropzone';
+import ChooseAFileButton from './components/ChooseAFileButton';
 
 ReactDOM.render(<Dropzone />, document.getElementById('dropzone'));
+ReactDOM.render(<ChooseAFileButton />, document.getElementById('chooseAFileButton'));
